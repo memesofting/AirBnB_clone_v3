e16552323f3559c49fbd280c21dcbebd568bea17
memesofting <bibitayoadekunle00@gmail.com>
2024-08-04 15:56:03 +0100

    initial tests passed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,12 +74,11 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id):
         """method to retrieve one object"""
-        
         pass
-    
+
     def count(self, cls=None):
         """method counts the number of objects in storage"""
         pass
